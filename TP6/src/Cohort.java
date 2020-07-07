cb0b926fa7d3566b434db4c7e9197c6a9930ab3c
Skanderza <67094393+Skanderza@users.noreply.github.com>
2020-07-07 10:14:32 +0200

    23

--- TP6/src/Cohort.java
+++ TP6/src/Cohort.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
 
-public class Cohort {
+import com.sun.tools.javac.util.List;
 
+public class Cohort<Student> {
+	 private final String name;
+	 
+     
+	
+	
+	 
+	public Cohort(String name) {
+		super();
+		this.name = name;
+	}
+
+
+
+
+	public void addStudent(Student students) {
+		ArrayList students1 = new ArrayList();
+		 students1.add("a");
+         
+}
+	 
+	 
+	 
+	 
+	 
+	 
 }
