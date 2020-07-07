8ba8d3d3d6b16bd3ba213a77e42643cdc5fd2ce5
abxlayedev <67095076+abxlayedev@users.noreply.github.com>
2020-07-07 10:58:01 +0200

    Modif TeachingUnitResult

---
+++ TP6/src/test.java
@@ -0,0 +1,21 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Double dd = 8.2;
+		Double test = null;
+		int blabla = 2;
+	
+		
+		
+		Grade grade = new Grade(20, false);
+		
+		System.out.println(grade.toString());
+		System.out.println(grade.isAbsent());
+	}
+	
+	
+
+}
